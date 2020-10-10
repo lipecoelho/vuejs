40bf7a3e604a48d51bd4fe98950a311667c8895d
lipecoelho <fellipegcoelho@gmail.com>
2020-10-09 21:00:00 -0300

    complete game

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -3,16 +3,12 @@ new Vue({
   data: {
     playerLife: 100,
     monsterLife: 100,
+    runningGame: false,
+    logs: [],
     playerScore: 0,
     monsterScore: 0,
     bgProgress: true,
     resultVisible: false,
-    runningGame: false,
-    playerWinner: false,
-    monsterWinner: false,
-    ko: false,
-    cure: 3,
-    attack: 3,
     activeClass: false,
     alertDanger: false,
     alertSuccess: false,
@@ -20,12 +16,15 @@ new Vue({
   },
   computed: {
     hasResult() {
+      if(this.playerLife == 0){this.monsterScore++ }
+      if(this.monsterLife == 0){this.playerScore++ }
       return this.playerLife == 0 || this.monsterLife == 0;
     }
   },
   methods: {
     startGame() {
       this.runningGame = true,
+      this.logs = [],
       this.playerLife = 100,
       this.monsterLife = 100.
       this.resultVisible = false,
@@ -35,21 +34,25 @@ new Vue({
       this.$refs.logs.firstElementChild.innerHTML = ""
     },
     attacking(special){
-      this.hurt('playerLife', 9, 15, false);
-      this.hurt('monsterLife', 7, 12, special);
+      this.hurt('monsterLife', 7, 12, special, 'Jogador', 'Monstro', 'alert-danger');
+      if(this.monsterLife > 0){
+        this.hurt('playerLife', 9, 15, false, 'Monstro', 'Jogador', 'alert-success');
+      }
     },
-    hurt(prop, min, max, special) {
+    hurt(prop, min, max, special, source, target, cls) {
       const plus = special ? 5 : 0;
       const hurt = this.getRandomInt(min + plus, max + plus);
       this[prop] = Math.max(this[prop] - hurt, 0); // nunca será negativo
+      this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls);
     },
     heal(min, max){
       const heal = this.getRandomInt(min, max);
       this.playerLife = Math.min(this.playerLife + heal, 100)
+      this.registerLog(`Jogador ganhou força de ${heal}.`, 'alert-success')
     },
     healAndHurt(){
       this.heal(10, 15);
-      this.hurt('playerLife', 7, 12, false);
+      this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'alert-danger');
     },
     getRandomInt(min, max) {
       const value = Math.random() * (max - min) + min;
@@ -57,6 +60,7 @@ new Vue({
       //return Math.floor(Math.random() * Math.floor(max));
     },
     resetGame() {
+      this.logs = [],
       this.runningGame = !this.runningGame, 
       this.playerLife = 100, 
       this.monsterLife = 100, 
@@ -66,6 +70,9 @@ new Vue({
       this.$refs.logs.style.display = "none"
       this.$refs.logs.firstElementChild.innerHTML = ""
     },
+    registerLog(text, cls) {
+      this.logs.unshift({text, cls})
+    }
   },
   watch: {
     hasResult(value) {
