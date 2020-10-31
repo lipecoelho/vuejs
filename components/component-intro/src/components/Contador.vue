9efd66c7ad879983103fdb454a080284362287c1
lipecoelho <fellipegcoelho@gmail.com>
2020-10-31 10:57:11 -0300

    new component

---
+++ components/component-intro/src/components/Contador.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="contador">	
+		<div>{{contador}}</div>
+		<button @click="contador++">+</button>
+		<button @click="contador--">-</button>
+	</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			contador: 0
+		}
+	}
+}
+</script>
