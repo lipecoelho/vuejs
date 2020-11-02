c10e9a90b8fb64d671285bc4e826ae1e0a6342d9
lipecoelho <fellipegcoelho@gmail.com>
2020-11-02 15:54:38 -0300

    comunication

---
+++ components/vuejs3/src/components/Contadores.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="contadores">
+		<Contador v-for="c in 5" :key="c" />
+	</div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+
+export default {
+	components: {"Contador": Contador}
+}
+</script>
+
+<style>
+
+</style>
