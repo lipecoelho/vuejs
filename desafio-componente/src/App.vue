4529c85e3b376ad87401d78024c77f0f99f3eb0b
lipecoelho <fellipegcoelho@gmail.com>
2020-11-14 09:10:11 -0300

    new desafio folder

---
+++ desafio-componente/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <app-menu />
+    <app-toolbar />
+    <app-carousel />
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+import Footer from "./components/Footer.vue";
+import Carousel from "./components/Carousel.vue";
+import Toolbar from "./components/Toolbar.vue";
+
+import Menu from "./components/Menu.vue";
+export default {
+  components: {
+    Footer,
+    appCarousel: Carousel,
+    appToolbar: Toolbar,
+    appMenu: Menu
+  }
+}
+</script>
